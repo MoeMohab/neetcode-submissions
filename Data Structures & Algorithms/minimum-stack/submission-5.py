de610aabe1c67888c3d723057571f621adfe9e69
Mohamed Mohab <67334959+MoeMohab@users.noreply.github.com>
2026-07-15 01:26:35 +0400

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, val, min):
+        self.val = val
+        self.min = min
+    
+class MinStack:
+
+    def __init__(self):
+        self.s = []
+
+    def push(self, val: int) -> None:
+        if not self.s:
+            self.s.append(Node(val, val))
+            return
+        self.s.append(Node(val, min(self.getMin(), val)))
+
+    def pop(self) -> None:
+        if self.s:
+            self.s.pop()
+
+    def top(self) -> int:
+        if not self.s:
+            return 0
+        return self.s[-1].val
+
+    def getMin(self) -> int:
+        if not self.s:
+            return 0
+        return self.s[-1].min
+        
